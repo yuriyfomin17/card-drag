869ee706644e03543d745d8902f20df3f1863410
yuriyfomin17 <fominyura2012@gmail.com>
2020-08-02 20:12:56 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import {DragDropContext, Droppable, Draggable} from "react-beautiful-dnd"
+import uuid from 'uuid-v4'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const itemsFromBackend = [
+    {id: uuid(), content: 'First task'},
+    {id: uuid(), content: 'Second task'}
+]
+const columnFromBackend =
+    {
+        [uuid()]: {
+            name: 'Todo',
+            items: itemsFromBackend
+        }
+
+    }
+const onDragEnd = (result, columns, setColumns) => {
+    if (!result.destination) return;
+    const {source, destination} = result;
+    const column = columns[source.droppableId]
+    const copiedItems = [...column.items]
+    const [removed] = copiedItems.splice(source.index, 1)
+    copiedItems.splice(destination.index, 0, removed)
+    setColumns({
+        ...columns,
+        [source.droppableId]: {
+            ...column,
+            items: copiedItems
+        }
+    })
 }
 
-export default App;
+export default function App() {
+    const [columns, setColumns] = useState(columnFromBackend)
+
+    return (
+
+        <div>
+            <DragDropContext onDragEnd={result => onDragEnd(result, columns, setColumns)}>
+                {Object.entries(columns).map(([id, column]) => {
+                    return (
+                        <Droppable droppableId={id} key={id}>
+                            {(provided, snapshot) => {
+                                return (
+                                    <div
+                                        {...provided.droppableProps}
+                                        ref={provided.innerRef}
+                                        style={{
+                                            background: snapshot.isDraggingOver ? 'lightblue' : 'lightgrey',
+                                            padding: 4,
+                                            width: 400,
+                                            minHeight: 500
+                                        }}
+                                    >
+                                        {column.items.map((item, index) => {
+                                            return (
+                                                <Draggable key={item.id} draggableId={item.id} index={index}>
+                                                    {(provided, snapshot) => {
+                                                        return (
+                                                            <div ref={provided.innerRef}
+                                                                 {...provided.draggableProps}
+                                                                 {...provided.dragHandleProps}
+                                                                 style={{
+                                                                     userSelect: 'none',
+                                                                     padding: 16,
+                                                                     margin: '0 0 8px 0',
+                                                                     minHeight: '50px',
+                                                                     backgroundColor: snapshot.isDragging ? '#263B4A' : '#456C86',
+                                                                     color: 'white',
+                                                                     ...provided.draggableProps.style
+                                                                 }}
+                                                            >
+                                                                {item.content}
+                                                            </div>
+                                                        )
+
+                                                    }}
+                                                </Draggable>
+                                            )
+                                        })}
+                                        {provided.placeholder}
+                                    </div>
+                                )
+                            }}
+                        </Droppable>
+                    )
+                })}
+
+            </DragDropContext>
+
+        </div>
+
+
+    )
+}
